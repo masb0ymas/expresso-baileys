bc82f7645562c4ffacb26ac258c91538958ff3f1
masb0ymas <n.fajri@outlook.com>
2023-06-11 11:27:19 +0700

    feat: add schema

---
+++ src/app/middleware/authorization.ts
@@ -0,0 +1,43 @@
+import { type NextFunction, type Request, type Response } from 'express'
+import { useToken } from 'expresso-hooks'
+import _ from 'lodash'
+import { env } from '~/config/env'
+import { logger } from '~/config/pino'
+import SessionService from '../service/session.service'
+
+/**
+ * Authorization
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
+async function authorization(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response<any, Record<string, any>> | undefined> {
+  const getToken = useToken.extract(req)
+
+  // verify token
+  const token = useToken.verify({
+    token: String(getToken),
+    secretKey: env.JWT_SECRET_ACCESS_TOKEN,
+  })
+
+  // check session from token header
+  const getSession = await SessionService.getByToken(String(getToken))
+
+  if (_.isEmpty(token?.data) || _.isEmpty(getSession)) {
+    logger.info('Permission: Unauthorized invalid jwt')
+
+    return res
+      .status(401)
+      .json({ code: 401, message: 'Unauthorized, invalid jwt' })
+  }
+
+  req.setState({ userLogin: token?.data })
+  next()
+}
+
+export default authorization
